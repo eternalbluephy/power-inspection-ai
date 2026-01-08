b66e14038d46e3c70b89038d8a50a063c3c4d2da
eternalbluephy <peihongyu123@126.com>
2026-01-08 14:19:51 +0800

    Sync

--- train.py
+++ train.py
@@ -1,15 +1,27 @@
+import multiprocessing as mp
+import platform
+
 from ultralytics import YOLO
 
-model = YOLO("yolo11n.pt", task="detect")
-results = model.train(
-    data="data.yaml",
-    epochs=100,
-    batch=16,
-    imgsz=960,
-    device=0,
-    workers=12,
-    seed=42,
-    pretrained=True,
-    cache=True,
-    verbose=True,
-)
+
+def main():
+    model = YOLO("yolo11n.pt", task="detect")
+    results = model.train(
+        data="data.yaml",
+        epochs=100,
+        batch=32,
+        imgsz=640,
+        device=0,
+        workers=12,
+        seed=42,
+        pretrained=True,
+        cache=True,
+        verbose=True,
+    )
+    print(results)
+
+
+if __name__ == "__main__":
+    if platform.system() == "Windows":
+        mp.freeze_support()
+    main()
